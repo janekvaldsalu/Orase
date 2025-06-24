6ae06ddd9bc2bd31e6a8ddf3529d6d8e54436cb6
janekvaldsalu <janek.valdsalu@gmail.com>
2025-06-25 02:23:46 +0300

    failid juhei

---
+++ data.js
@@ -0,0 +1,395 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-vannituba",
+      "name": "Vannituba",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.607312940855902,
+          "pitch": 0.34856035153777043,
+          "rotation": 0,
+          "target": "4-esik-kki"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-magamistuba",
+      "name": "Magamistuba",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.992428308230739,
+          "pitch": 0.13816949204931994,
+          "rotation": 0,
+          "target": "5-esik-esikusse"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-elutuba",
+      "name": "Elutuba",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.721326424296185,
+          "pitch": 0.18608927479976956,
+          "rotation": 0,
+          "target": "3-kk"
+        },
+        {
+          "yaw": 2.0292757075450494,
+          "pitch": 0.0924345196059484,
+          "rotation": 0,
+          "target": "4-esik-kki"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-kk",
+      "name": "Köök",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.6292032673446455,
+          "pitch": 0.23163777610433023,
+          "rotation": 6.283185307179586,
+          "target": "2-elutuba"
+        },
+        {
+          "yaw": -2.778999074628956,
+          "pitch": 0.2650081845665113,
+          "rotation": 7.853981633974483,
+          "target": "4-esik-kki"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-esik-kki",
+      "name": "Esik Kööki",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.871628144881381,
+          "pitch": 0.4652760811130534,
+          "rotation": 0,
+          "target": "3-kk"
+        },
+        {
+          "yaw": 0.9165698217019322,
+          "pitch": 0.535699035456787,
+          "rotation": 0,
+          "target": "2-elutuba"
+        },
+        {
+          "yaw": 2.5398539672097966,
+          "pitch": 0.39308638455118405,
+          "rotation": 0,
+          "target": "5-esik-esikusse"
+        },
+        {
+          "yaw": -2.6464327524192264,
+          "pitch": 0.24595750267616978,
+          "rotation": 0,
+          "target": "0-vannituba"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-esik-esikusse",
+      "name": "Esik Esikusse",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.4644762065223542,
+          "pitch": 0.3949586233332667,
+          "rotation": 0,
+          "target": "1-magamistuba"
+        },
+        {
+          "yaw": -0.7636132200972217,
+          "pitch": 0.42075275291642633,
+          "rotation": 0,
+          "target": "4-esik-kki"
+        },
+        {
+          "yaw": -2.3610066224120274,
+          "pitch": 0.3005407481229341,
+          "rotation": 0,
+          "target": "6-esik"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-esik",
+      "name": "Esik",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": -1.6263782814432943,
+        "pitch": 0.06798005210397484,
+        "fov": 1.3680985485926078
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.9306043532811579,
+          "pitch": 0.23417340055289415,
+          "rotation": 0,
+          "target": "5-esik-esikusse"
+        },
+        {
+          "yaw": -2.453779891563812,
+          "pitch": 0.1668717307287384,
+          "rotation": 0,
+          "target": "7-kontor"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-kontor",
+      "name": "Kontor",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.869138392268656,
+          "pitch": 0.14461862171322437,
+          "rotation": 0,
+          "target": "6-esik"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
